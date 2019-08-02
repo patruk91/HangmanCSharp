c0110f2286240c6f49a312066b9c71affaa7a663
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-02 19:57:21 +0200

    Implement class Country: add properties for capital and country name

--- src/model/Country.cs
+++ src/model/Country.cs
@@ -2,6 +2,18 @@
 {
     public class Country
     {
-        
+        public Country(string capitalName, string countryName)
+        {
+            CapitalName = capitalName;
+            CountryName = countryName;
+        }
+
+        public string CapitalName { get; }
+        public string CountryName { get; }
+
+        public override string ToString()
+        {
+            return $"Country: {CountryName}, Capital: {CapitalName}";
+        }
     }
 }
