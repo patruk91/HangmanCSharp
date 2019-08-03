13bb235a070c90f119bab106312c66e634fa3646
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-03 10:53:50 +0200

    Implement method to display message

--- src/view/View.cs
+++ src/view/View.cs
@@ -1,7 +1,12 @@
-﻿namespace HangmanCSharp.view
+﻿using System;
+
+namespace HangmanCSharp.view
 {
     public class View
     {
-        
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
     }
 }
