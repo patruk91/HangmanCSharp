8689d904e9e9e341b639d1c76a285f5d4dfaaebf
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-03 08:10:45 +0200

    Add player class: constructor, and getters, setters methods

--- src/model/Player.cs
+++ src/model/Player.cs
@@ -1,7 +1,25 @@
-﻿namespace HangmanCSharp.model
+﻿using System.Collections.Generic;
+
+namespace HangmanCSharp.model
 {
     public class Player
     {
+        private List<string> GoodGuesses;
+        private List<string> BadGuesses;
+
+        public Player(string name)
+        {
+            GoodGuesses = new List<string>();
+            BadGuesses = new List<string>();
+            Tries = 0;
+            Lives = 10;
+            Name = name;
+        }
+
+        public int Tries { get; private set; }
+        public int Lives { get; private set; }
+        public string Name { get; set; }
+
         
     }
 }
