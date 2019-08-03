5d594a2f9909d7e5edd8491fa82b3b7ad81d12df
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-03 08:48:13 +0200

    Create Score class, and implement override method ToString

--- src/model/Score.cs
+++ src/model/Score.cs
@@ -1,7 +1,27 @@
-﻿namespace HangmanCSharp.model
+﻿using System;
+
+namespace HangmanCSharp.model
 {
     public class Score
     {
-        
+        private readonly string _name;
+        private readonly DateTime _dateTime;
+        private readonly int _time;
+        private readonly int _tries;
+        private readonly int _capital;
+
+        public Score(string name, DateTime dateTime, int time, int tries, int capital)
+        {
+            _name = name;
+            _dateTime = dateTime;
+            _time = time;
+            _tries = tries;
+            _capital = capital;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name} | {_dateTime.Date} | {_time} | {_tries} | {_capital}";
+        }
     }
 }
