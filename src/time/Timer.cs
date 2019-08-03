dd31b6b63debc163e8326ac26cdc3d88b6433973
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-03 08:40:02 +0200

    Implement class Timer to :start, end and time calculations

---
+++ src/time/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HangmanCSharp.time
+{
+    public class Timer
+    {
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+        
+        public void StartTime()
+        {
+            _stopwatch.Start();
+        }
+
+        public void EndTime()
+        {
+            _stopwatch.Stop();
+        }
+
+        public int TotalTimeTakenInSeconds()
+        {
+            return Convert.ToInt32(_stopwatch.ElapsedMilliseconds / 1000);
+        }
+
+
+
+    }
+}
