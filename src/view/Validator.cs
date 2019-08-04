914bf234ef87da26877707912e71e84c19b4d81e
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-04 14:43:28 +0200

    Check is user guess correct word

--- src/view/Validator.cs
+++ src/view/Validator.cs
@@ -1,10 +1,18 @@
-﻿namespace HangmanCSharp.view
+﻿using System;
+using HangmanCSharp.model;
+
+namespace HangmanCSharp.view
 {
     public class Validator
     {
         public bool IsInputEmpty(string input)
         {
             return string.IsNullOrEmpty(input);
+        }
+
+        public bool IsACorrectWord(Country secretCountry, string userGuess)
+        {
+            return string.Equals(secretCountry.CapitalName, userGuess, StringComparison.CurrentCultureIgnoreCase);
         }
     }
 }
