bd3b8fb662a5b198236ebbe2f79250d84f3d9f21
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-04 09:41:40 +0200

    Get random country and hide secret word by dash function

---
+++ src/model/Word.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HangmanCSharp.model
+{
+    public class Word
+    {
+        private readonly Random _random;
+
+        public Word(Random random)
+        {
+            this._random = random;
+        }
+
+        public Country GetRandomCountry(List<Country> countries)
+        {
+            const int counterArraySize = 1;
+            int randomIndex = _random.Next(0, countries.Count - counterArraySize);
+            return countries[randomIndex];
+        }
+
+
+        public string MakeDashWord(Player player, Country secretCountry)
+        {
+            string dashedWord = "";
+            string capital = secretCountry.CapitalName.ToUpper();
+            for (int i = 0; i < capital.Length; i++)
+            {
+                string letter = char.ToString(capital[i]);
+                dashedWord += player.GetGoodGuesses.Contains(letter) ? $"{letter}" : letter == " " ? "   " : " _ ";
+            }
+
+            return dashedWord;
+        }
+    }
+}
