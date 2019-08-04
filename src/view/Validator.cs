4ceb1da01e389085212d6348967a5e5c9fa2a66b
Patryk Lichwala <lipatryk91@gmail.com>
2019-08-04 08:16:24 +0200

    Add Validator class: check if input string is empty

---
+++ src/view/Validator.cs
@@ -0,0 +1,10 @@
+﻿namespace HangmanCSharp.view
+{
+    public class Validator
+    {
+        public bool IsInputEmpty(string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+    }
+}
